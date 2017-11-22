af29281dcb69df35fab6bca250c5682b75431bb9
Joe Widi <jwidi@outlook.com>
2017-11-22 15:44:25 -0600

    Added Player Class w/ basic movement.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,87 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+class Enemy {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  constructor(x, y) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
+    this.x = x;
+    this.y = y;
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt, x, y) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    //this.x = x * dt;
+    //this.y = x * dt;
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  // Draw the enemy on the screen, required method for game
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+  constructor(avatar) {
+    this.sprite = 'images/' + avatar + '.png';
+    this.x = 200; //100 increments per square
+    this.y = 405; //80 increments per square
+  }
 
+  // Update the player's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+  }
+
+  // Draw the player on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput(e) {
+    switch (e) {
+      case "up":
+        this.y -= 80;
+        break;
+      case "down":
+        this.y += 80;
+        break;
+      case "left":
+        this.x -= 100;
+        break;
+      case "right":
+        this.x += 100;
+        break;
+    }
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+let allEnemies = new Array(
+    new Enemy(0, 70)
+  , new Enemy(0, 150)
+  , new Enemy(0, 230)
+);
+let player = new Player("char-boy");
+let position
+
 // Place the player object in a variable called player
-
-
+function startGame() {
+  allEnemies.forEach((enemy) => { enemy.render(); });
+  player.render();
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -43,4 +94,5 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+    startGame();
 });
